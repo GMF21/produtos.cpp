52d59537d8c3f8928bd59caede23578fbbd200b1
GNC <145757613+GMF21@users.noreply.github.com>
2024-09-20 20:32:30 +0000

    Update produtos terminado.cpp

--- produtos terminado.cpp
+++ produtos terminado.cpp
@@ -8,40 +8,40 @@ struct produto {
     int quantidade;
 };
 
-void adicionarProduto(produto produtos[], int& quantidadeAtual) {
+void adicionarProduto(produto produtos[], int& quantidadeAtual) { // funcao para adicionar produtos
     cout << "Nome do produto: ";
-    cin >> produtos[quantidadeAtual].nome;
+    cin >> produtos[quantidadeAtual].nome; //o array de produtos guarda as varias cenas da estrutura (nome)
     cout << "Preço do produto: ";
-    cin >> produtos[quantidadeAtual].preco;
+    cin >> produtos[quantidadeAtual].preco; // (preco)
     cout << "Quantidade em estoque: ";
-    cin >> produtos[quantidadeAtual].quantidade;
+    cin >> produtos[quantidadeAtual].quantidade; // (quantidade)
     quantidadeAtual++;
 }
 
-void exibirProdutos(const produto produtos[], int quantidadeAtual) {
-    for (int i = 0; i < quantidadeAtual; i++) {
+void exibirProdutos(const produto produtos[], int quantidadeAtual) { // funcao para mostrar os produtos
+    for (int i = 0; i < quantidadeAtual; i++) { // ciclo for para smpre q exibir mais que um produto numeralos 
         cout << "Produto " << i+1 << ":" << endl;
-        cout << "Nome: " << produtos[i].nome << endl;
-        cout << "Preço: " << produtos[i].preco << endl;
-        cout << "Quantidade: " << produtos[i].quantidade << endl;
+        cout << "Nome: " << produtos[i].nome << endl; // mostrar o nome 
+        cout << "Preço: " << produtos[i].preco << endl; // mostrar o preco
+        cout << "Quantidade: " << produtos[i].quantidade << endl; // mostrar quantidade
         cout << "-------------------------" << endl;
     }
 }
 
-float calcularValorTotal(const produto produtos[], int quantidadeAtual) {
-    float valorTotal = 0.0;
-    for (int i = 0; i < quantidadeAtual; i++) {
-        valorTotal += produtos[i].preco * produtos[i].quantidade;
+float calcularValorTotal(const produto produtos[], int quantidadeAtual) { // const produto produtos - array de produtos
+    float valorTotal = 0.0;  
+    for (int i = 0; i < quantidadeAtual; i++) {  // inicia com a variavel para o total como 0 e precorre o array de produtos
+        valorTotal += produtos[i].preco * produtos[i].quantidade; // multiplicando  o preço pela quantidade e soma o valor
     }
-    return valorTotal;
+    return valorTotal; // retorna o valor do stock
 }
 
 int main() {
-    produto maxProdutos[100];
-    int quantidadeAtual = 0;
+    produto maxProdutos[100]; // maximiza a quantidade dos produtos que podemos adicionar que é 100
+    int quantidadeAtual = 0; //armazena o numero de produtos no stock
     int escolha;
 
-    do {
+    do {  // precorre o codigo enquanto a escolha for difrente ou = 0
         cout << "Escolha uma opção:" << endl;
         cout << "1. Adicionar Produto" << endl;
         cout << "2. Exibir Produtos" << endl;
